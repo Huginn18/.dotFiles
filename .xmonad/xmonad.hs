1d87561133d9da057dea4b01dcb8f99cc12ab61e
huginn <huginn@huginn.me>
2021-03-23 08:15:38 +0100

    added basic config
    - focus follow
    - border settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,11 +8,19 @@ import XMonad.Util.SpawnOnce
 -- --- -- - -- ---
 -- VARIABLES
 -- --- -- - -- ---
+myBorderWidth = 2
+
+myNormalBorderColor = "#928374"
+myFocusedBorderColor = "#ebdbb2"
+
 myModMask :: KeyMask
 myModMask = mod4Mask -- super/windows key | default: mod1Mask left alt
 
 myTerminal :: String
 myTerminal="alacritty"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
 
 -- --- -- - -- ---
 -- HOOKS
@@ -21,10 +29,23 @@ myStartupHook = do
     spawnOnce "nitrogen --restore &"
     spawnOnce "compton &"
 
+-- --- -- - -- ---
+-- HOTKEYS
+-- --- -- - -- ---
+
+
+
+-- --- -- - -- --- -- - -- ---
+-- MAIN
 main = xmonad defaultConfig
-        { 
-          modMask = mod4Mask -- Use Super instead of Alt
+        {
+          borderWidth = myBorderWidth 
+        , normalBorderColor = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+
+        , modMask = mod4Mask -- Use Super instead of Alt
         , terminal = myTerminal
+        , focusFollowsMouse = myFocusFollowsMouse
         -- hooks
         , startupHook = myStartupHook
         }
