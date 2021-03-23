703583bc6405f3e194191ef06eb117b69f74fadb
huginn <huginn@huginn.me>
2021-03-23 16:23:57 +0100

    added layout Hhook and hotkeys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,15 +1,38 @@
+import System.Exit
+
+
+-- --- -- - -- --- -- - -- --- -- - -- ---
+-- IMPORTS: XMonad
+-- --- -- - -- --- -- - -- --- -- - -- ---
 import XMonad
+import qualified XMonad.StackSet as W
+
+-- --- -- - -- --- -- - -- --- -- - -- ---
+-- IMPORTS: ACTIONS
+-- --- -- - -- --- -- - -- --- -- - -- ---
+import XMonad.Actions.CycleWS
+
+-- --- -- - -- --- -- - -- --- -- - -- ---
+-- IMPORTS: LAYOUT
+-- --- -- - -- --- -- - -- --- -- - -- ---
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.Spacing
+import XMonad.Layout.Grid
 
 -- --- -- - -- --- -- - -- --- -- - -- ---
 -- IMPORTS: HOOKS
 -- --- -- - -- --- -- - -- --- -- - -- ---
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 
 -- --- -- - -- ---
--- IMPORTS: UTIL
+-- IMPORTS: UTILS
 -- --- -- - -- ---
+import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run
+
+
 
 -- --- -- - -- ---
 -- VARIABLES
@@ -36,10 +59,54 @@ myStartupHook = do
     spawnOnce "compton &"
 
 -- --- -- - -- ---
+-- LAYOUT
+-- --- -- - -- ---
+myLayoutHook = avoidStruts $ layoutSet
+    where
+
+        -- --- -- - --
+        -- variables
+        -- --- -- - --
+        nMaster = 1     -- number of windows in master column
+        ratio12 = 1/2   
+        delta = 3/100   -- percent of screen to increment when resizing
+
+        --
+        tiledLayout = spacing 8 $ Tall nMaster delta ratio12
+        layoutGrid = spacing 8 $ Grid
+        layoutTall = Tall nMaster delta ratio12
+        layoutSet = toggleLayouts Full (layoutTall ||| layoutGrid ||| Full)
+
+-- --- -- - -- ---
 -- HOTKEYS
 -- --- -- - -- ---
+myKeys :: [(String, X ())]
+myKeys = 
+    [ ("M-S-q", io (exitWith ExitSuccess))
+    , ("M-S-c", kill)
+    , ("M-q", spawn "xmonad --recompile; killall xmobar; xmonad --restart")
+    , ("M-p", spawn "dmenu_run")
+    , ("M-S-<Return>", spawn(myTerminal))
+ 
+    -- Layout Controls
+    , ("M-<Return>", windows W.swapMaster)
+    , ("M-b", sendMessage ToggleStruts)
+    
+    , ("M-k", windows W.focusUp)
+    , ("M-j", windows W.focusDown)
+    , ("M-S-k", windows W.swapUp)
+    , ("M-S-j", windows W.swapDown)
 
+    , ("M-h", sendMessage Shrink)
+    , ("M-l", sendMessage Expand)
+    
+    , ("M-C-k", sendMessage (IncMasterN 1))
+    , ("M-C-j", sendMessage (IncMasterN (-1))) 
 
+    -- Workspace Controls
+    , ("M-<R>", nextWS)
+    , ("M-<L>", prevWS)
+   ]
 
 -- --- -- - -- --- -- - -- ---
 -- MAIN
@@ -56,4 +123,6 @@ main = do
         , focusFollowsMouse = myFocusFollowsMouse
           -- hooks
         , startupHook = myStartupHook
-        } 
+        , layoutHook = myLayoutHook
+        , handleEventHook = docksEventHook
+        }`additionalKeysP` myKeys 
