5dbec9dc091fa563a84839f62835ade620e82cf1
huginn <huginn@huginn.me>
2021-03-23 04:46:58 +0100

    added compton and nitrogen to XMonad startupHook

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,31 @@
 import XMonad
 
+-- --- -- - -- ---
+-- Utils
+-- --- -- - -- ---
+import XMonad.Util.SpawnOnce
+
+-- --- -- - -- ---
+-- VARIABLES
+-- --- -- - -- ---
+myModMask :: KeyMask
+myModMask = mod4Mask -- super/windows key | default: mod1Mask left alt
+
+myTerminal :: String
+myTerminal="gnome-terminal"
+
+-- --- -- - -- ---
+-- HOOKS
+-- --- -- - -- ---
+myStartupHook = do
+    spawnOnce "nitrogen --restore &"
+    spawnOnce "compton &"
+
 main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
-        , terminal = "gnome-terminal"
-        -- more changes
+        { 
+          modMask = mod4Mask -- Use Super instead of Alt
+        , terminal = myTerminal
+        -- hooks
+        , startupHook = myStartupHook
         }
 
